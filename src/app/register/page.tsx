d90cb99a3569af08aa2b01e56ba58524899b12e1
Jin <fauzipribadi18@gmail.com>
2025-06-14 19:08:48 +0800

    feat(register): slicing ui register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import TextInput from '@/components/Input/TextInput'
+import Image from 'next/image'
+import React from 'react'
+
+function Register() {
+    return (
+        <div className='flex items-center w-screen h-screen justify-between font-poppins'>
+            {/* Left Column */}
+            <div className='w-2/5 h-full p-16 flex flex-col gap-16'>
+                <div className='flex flex-row items-center gap-4'>
+                    <Image
+                        src="/assets/icons/favicon.png"
+                        width={120}
+                        height={120}
+                        alt="logo" />
+                    <div className="flex flex-col">
+                        <p className='text-2xl text-absensi-primary font-bold'>
+                            Sekolahku
+                        </p>
+                        <p className='text-primary'>
+                            Sekolahku, sekolahmu, sekolah kita semu
+                        </p>
+                    </div>
+                </div>
+
+                <h1 className='text-xl font-bold text-primary'>
+                    Daftar <br /> ke Sekolahku
+                </h1>
+
+                <div className='flex flex-col gap-4'>
+                    <TextInput
+                        label='Nama'
+                        type='text'
+                        name='name'
+                        placeholder='Masukkan nama'
+                    />
+
+                    <TextInput
+                        label='Email'
+                        type='email'
+                        name='email'
+                        placeholder='Masukkan email'
+                    />
+
+                    <TextInput
+                        label='Password'
+                        type='password'
+                        name='password'
+                        placeholder='Masukkan password'
+                    />
+                </div>
+
+                <div>
+                    <button className='bg-absensi-primary text-white py-3 rounded-lg w-full'>
+                        Daftar
+                    </button>
+                    <div>
+                        <p className='text-primary text-center mt-4'>
+                            Sudah punya akun? <a href="/login" className='text-absensi-primary'>Masuk disini</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Right Column */}
+            <div className='w-3/5 h-full'>
+                <Image
+                    src="/assets/images/banner2.png"
+                    alt='banner'
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Register
